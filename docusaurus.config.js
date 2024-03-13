351cb8a92267f07a950c164a72054241b5dc1664
Greg Glazewski <g.glazewski@gmail.com>
2024-03-13 14:16:04 +0100

    enable gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,10 @@ const config = {
             'classic',
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
+                gtag: {
+                    trackingID: 'G-QJLMLJ2XXC',
+                    anonymizeIP: true,
+                },
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
