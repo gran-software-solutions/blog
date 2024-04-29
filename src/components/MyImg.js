cd53e84d8704c0be15b383830140f3d19e618104
Dusan Odalovic <dusan.odalovic@gransoftware.de>
2024-04-29 09:13:53 +0200

    Sheetty Teams Intro

--- src/components/MyImg.js
+++ src/components/MyImg.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 function MyImg(props) {
+    const imgStyle = props.width ? { width: `${props.width}px` } : {};
     if (props.title !== undefined) {
         return (
             <center>
                 <figure>
 
-                    <img src={props.src} alt={props.alt}/>
+                    <img src={props.src} alt={props.alt} style={imgStyle}/>
                     <figcaption style={{textAlign: "center"}}>{props.title}</figcaption>
                 </figure>
 
@@ -14,7 +15,7 @@ function MyImg(props) {
 
         );
     } else {
-        return <img src={props.src} alt={props.alt}/>;
+        return <img src={props.src} alt={props.alt} style={imgStyle}/>;
     }
 }
 
